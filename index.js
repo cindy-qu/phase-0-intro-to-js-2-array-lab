a01fd5a7da39f118ba4558c516d0ea7c5924b5be
Cindy Qu <cindyqu42@gmail.com>
2022-08-30 08:40:52 -0700

    commit tree c5540bbe6e73dd5260209eb61840a3472cc4bff2

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const newCats = [...cats, name];
+}
+
+
+function prependCat(name) {
+    const prependCats = [name, ...cats];
+}
+
+function removeLastCat(name) {
+    cats.slice(2)
+}
